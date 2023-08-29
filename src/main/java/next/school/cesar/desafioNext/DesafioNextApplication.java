e883849f789be5dcd934b955548f9195b6a4c052
Eduardo Morais <dudu.vgvm@gmail.com>
2023-08-29 19:09:47 -0300

    Projeto Inicial

---
+++ src/main/java/next/school/cesar/desafioNext/DesafioNextApplication.java
@@ -0,0 +1,13 @@
+package next.school.cesar.desafioNext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioNextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioNextApplication.class, args);
+	}
+
+}
